31a21dadd0f5eeeffca7b615325dea34c8f9e43b
Jacob <jacobjk01@gmail.com>
2019-03-09 12:47:23 -0800

    Added app

---
+++ Jukebox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Jukebox
+//
+//  Created by Jacob Kim on 3/9/19.
+//  Copyright © 2019 Jacob Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
