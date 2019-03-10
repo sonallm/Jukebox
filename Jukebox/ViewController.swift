efe0c3717eecbccc9f07dd97a4a51069f76d0cca
Jacob <jacobjk01@gmail.com>
2019-03-09 16:45:28 -0800

    Adding to Firebase capability

--- Jukebox/ViewController.swift
+++ Jukebox/ViewController.swift
@@ -7,18 +7,47 @@
 //
 
 import UIKit
-import Firebase
+import Alamofire
 import FirebaseDatabase
 
 class ViewController: UIViewController {
-
-    var ref: DatabaseReference! = Database.database().reference()
+    
+    @IBOutlet weak var SongTextField: UITextField!
+    @IBOutlet weak var SubmitButton: UIButton!
+    
+    var searchURL = "https://api.spotify.com/v1/search?q=Madeon&type=track%2C%20album%2C%20artist"
+    typealias JSONStandard = [String: AnyObject]
+    
+    var ref: DatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        ref = Database.database().reference()
+        callAlamo(url: searchURL)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    func callAlamo(url: String) {
+        AF.request(url).responseJSON(completionHandler: {
+            response in
+            self.parseData(JSONData: response.data!)
+        })
+    }
+    
+    func parseData(JSONData: Data) {
+        do {
+            var readableJSON = try JSONSerialization.jsonObject(with: JSONData, options: .mutableContainers) as? JSONStandard
+            print(readableJSON)
+        }
+        catch {
+            print(error)
+        }
+    }
+    
+    @IBAction func SubmitButtonPressed(_ sender: Any) {
+        var info = SongTextField.text
+        self.ref.child("songs").setValue(["songTitle": info])
+    }
+    
 }
 
--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+# platform :ios, '10.0'
 
 target 'Jukebox' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
@@ -9,5 +9,7 @@ target 'Jukebox' do
 
 pod 'Firebase/Core'
 pod 'Firebase/Database'
+pod 'Alamofire', '~> 5.0.0-beta.3'
+pod 'OAuthSwift', '~> 1.2.0'
 
 end
